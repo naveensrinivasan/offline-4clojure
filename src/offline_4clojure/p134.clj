7c0902091b896443ae784e73716e542681c5af3f
Naveen <naveensrinivasan@yahoo.com>
2014-10-13 21:13:59 -0400

    Worked on Contains

--- src/offline_4clojure/p134.clj
+++ src/offline_4clojure/p134.clj
@@ -1,17 +1,18 @@
 ; A nil key - Elementary
-; Write a function which, given a key and map, returns true <a href="http://en.wikipedia.org/wiki/If_and_only_if">iff</a> the map contains an entry with that key and its value is nil.
+; Write a function which, given a key
+; and map, returns true <a href="http://en.wikipedia.org/wiki/If_and_only_if">iff</a>
+; the map contains an entry with that key and its value is nil.
 ; tags - maps
-; restricted - 
+; restricted -
 (ns offline-4clojure.p134
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(defn doesItContain [k m]
+  (contains? m k))
 
 (defn -main []
   (are [x] x
-(true?  (__ :a {:a nil :b 2}))
-(false? (__ :b {:a nil :b 2}))
-(false? (__ :c {:a nil :b 2}))
+(true?  ( doesItContain :a {:a nil :b 2}))
+(false? (doesItContain :b {:a nil :b 2}))
+(false? (doesItContain :c {:a nil :b 2}))
 ))
